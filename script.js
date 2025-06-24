f7324216d514e35637d90e973fc5645ab4ee1fbd
Xori <xori93@aol.com>
2025-06-23 21:26:09 -0400

    js not working

---
+++ script.js
@@ -0,0 +1,40 @@
+const buttons = document.querySelectorAll('.mood');
+const moodTitle = document.getElementById('#mood_title');
+const mood_message = document.getElementById('#mood_message');
+const body = document.body;
+
+const moodMap = {
+  happy: {
+     color: '#ffe066',
+      title: 'You are feeling Happy 😊',
+      message: 'Smile big! Today is full of sunshine and good vibes.'
+    },
+    sad: {
+      color: '#a0c4ff',
+      title: 'You are feeling Sad 😢',
+      message: 'It’s okay to feel down. Be gentle with yourself today.'
+    },
+    excited: {
+      color: '#ffadad',
+      title: 'You are feeling Excited 🤩',
+      message: 'So much energy! Let that excitement guide your day!'
+    },
+    chill: {
+      color: '#caffbf',
+      title: 'You are feeling Chill 😌',
+      message: 'Deep breaths. Peaceful moments are the best ones.'
+    }
+  
+};
+
+buttons.forEach(button => {
+  button.addEventListener('click', () => {
+    const mood = button.getAttribute('data-mood');
+    const { color, title, message } = moodMap[mood];
+
+    body.style.background = color;
+
+    moodTitle.textContent = title;
+    mood_message.textContent = message;
+  })
+});
